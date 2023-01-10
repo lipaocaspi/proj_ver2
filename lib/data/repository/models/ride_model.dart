41787e7a47bc33e3582682ee8571d3383a506363
Liliana Castellanos <lilipao140799@gmail.com>
2023-01-10 11:31:10 -0500

    up_model

--- lib/data/repository/models/ride_model.dart
+++ lib/data/repository/models/ride_model.dart
@@ -11,7 +11,11 @@ class Ride {
     required this.userId,
     required this.id,
     required this.start,
+    required this.latS,
+    required this.lonS,
     required this.end,
+    required this.latE,
+    required this.lonE,
     required this.dateAndTime,
     required this.vehicle,
     required this.room,
@@ -24,7 +28,11 @@ class Ride {
   int userId;
   int id;
   String start;
+  String latS;
+  String lonS;
   String end;
+  String latE;
+  String lonE;
   String dateAndTime;
   String vehicle;
   String room;
@@ -37,7 +45,11 @@ class Ride {
         userId: json["userId"],
         id: json["id"],
         start: json["start"],
+        latS: json["latS"],
+        lonS: json["lonS"],
         end: json["end"],
+        latE: json["latE"],
+        lonE: json["lonE"],
         dateAndTime: json["dateAndTime"],
         vehicle: json["vehicle"],
         room: json["room"],
@@ -51,7 +63,11 @@ class Ride {
         "userId": userId,
         "id": id,
         "start": start,
+        "latS": latS,
+        "lonS": lonS,
         "end": end,
+        "latE": latE,
+        "lonE": lonE,
         "dateAndTime": dateAndTime,
         "vehicle": vehicle,
         "room": room,
@@ -61,3 +77,65 @@ class Ride {
         "state": state,
       };
 }
+
+// class Start {
+//   Start({
+//     required this.name,
+//     required this.geo,
+//   });
+
+//   String name;
+//   Geo geo;
+
+//   factory Start.fromJson(Map<String, dynamic> json) =>
+//   Start(
+//     name: json["name"],
+//     geo: Geo.fromJson(json["geo"]),
+//   );
+
+//   Map<String, dynamic> toJson() => {
+//     "name": name,
+//     "geo": geo.toJson(),
+//   };
+// }
+
+// class End {
+//   End({
+//     required this.name,
+//     required this.geo,
+//   });
+
+//   String name;
+//   Geo geo;
+
+//   factory End.fromJson(Map<String, dynamic> json) =>
+//   End(
+//     name: json["name"],
+//     geo: Geo.fromJson(json["geo"]),
+//   );
+
+//   Map<String, dynamic> toJson() => {
+//     "name": name,
+//     "geo": geo.toJson(),
+//   };
+// }
+
+// class Geo {
+//   Geo({
+//     required this.lat,
+//     required this.lon,
+//   });
+
+//   String lat;
+//   String lon;
+
+//   factory Geo.fromJson(Map<String, dynamic> json) => Geo(
+//     lat: json["lat"],
+//     lon: json["lon"],
+//   );
+
+//   Map<String, dynamic> toJson() => {
+//     "lat": lat,
+//     "lon": lon,
+//   };
+// }
