1f7a8621cc2e9698bf8f7a2f4f1d33d78f09739b
Liliana Castellanos <lilipao140799@gmail.com>
2023-01-21 23:26:06 -0500

    mainpage_fin

--- lib/MainPage/main_page_screen.dart
+++ lib/MainPage/main_page_screen.dart
@@ -13,7 +13,6 @@ import 'package:proj_ver1/MainPage/components/ride_page.dart';
 import 'package:proj_ver1/data/repository/models/ride_model.dart';
 import 'package:proj_ver1/data/repository/models/user_model.dart';
 import 'package:proj_ver1/SettingsPage/settings_page_screen.dart';
-import 'package:proj_ver1/data/repository/models/chat_model.dart';
 import 'package:proj_ver1/UserRidesPage/user_rides_page_screen.dart';
 
 class MainPage extends StatefulWidget {
@@ -24,7 +23,6 @@ class MainPage extends StatefulWidget {
   List<Ride> ridesU = [];
   List<Ride> ridesP = [];
   final List<Users> _users = [];
-  final List<Chat> chats = [];
 
   @override
   State<MainPage> createState() => MainPageState();
@@ -44,9 +42,16 @@ class MainPageState extends State<MainPage> {
     toast.init(context);
   }
 
+  clearSearch() {
+    _controllerSearchStart.clear();
+    _controllerSearchEnd.clear();
+    _controllerSearchDate.clear();
+  }
+
   loadRides() async {
     widget.ridesA.clear();
     widget.ridesU.clear();
+    widget.ridesP.clear();
     final response = await http.get(Uri.parse("http://192.168.1.40:3000/rides"));
 
     if (response.statusCode == 200) {
@@ -55,6 +60,7 @@ class MainPageState extends State<MainPage> {
       setState(() {
         widget.ridesA.addAll(rides.where((element) => element.state == false && widget.id != element.userId));
         widget.ridesU.addAll(rides.where((element) => widget.id == element.userId));
+        widget.ridesP.addAll(rides.where((element) => element.userP1Id == widget.id || element.userP2Id == widget.id || element.userP3Id == widget.id || element.userP4Id == widget.id));
       });
       final upSnack = SnackBar(
         content: Text("Viajes actualizados"),
@@ -97,10 +103,66 @@ class MainPageState extends State<MainPage> {
     }
   }
 
+  loadFilterCloserCar() async {
+    widget.ridesA.clear();
+    final response =
+        await http.get(Uri.parse("http://192.168.1.40:3000/rides?vehicle=Autom%C3%B3vil&_sort=dateAndTime&_order=asc"));
+
+    if (response.statusCode == 200) {
+      List<dynamic> myRides = json.decode(utf8.decode(response.bodyBytes));
+      List<Ride> rides = myRides.map((e) => Ride.fromJson(e)).toList();
+      setState(() {
+        widget.ridesA.addAll(rides.where((element) => element.state == false && widget.id != element.userId));
+      });
+    }
+  }
+
+  loadFilterCloserMoto() async {
+    widget.ridesA.clear();
+    final response =
+        await http.get(Uri.parse("http://192.168.1.40:3000/rides?vehicle=Motocicleta&_sort=dateAndTime&_order=asc"));
+
+    if (response.statusCode == 200) {
+      List<dynamic> myRides = json.decode(utf8.decode(response.bodyBytes));
+      List<Ride> rides = myRides.map((e) => Ride.fromJson(e)).toList();
+      setState(() {
+        widget.ridesA.addAll(rides.where((element) => element.state == false && widget.id != element.userId));
+      });
+    }
+  }
+
   loadFilterFurtherDate() async {
     widget.ridesA.clear();
     final response =
         await http.get(Uri.parse("http://192.168.1.40:3000/rides?_sort=dateAndTime&_order=desc"));
+
+    if (response.statusCode == 200) {
+      List<dynamic> myRides = json.decode(utf8.decode(response.bodyBytes));
+      List<Ride> rides = myRides.map((e) => Ride.fromJson(e)).toList();
+      setState(() {
+        widget.ridesA.addAll(rides.where((element) => element.state == false && widget.id != element.userId));
+      });
+    }
+  }
+
+  loadFilterFurtherCar() async {
+    widget.ridesA.clear();
+    final response =
+        await http.get(Uri.parse("http://192.168.1.40:3000/rides?vehicle=Autom%C3%B3vil&_sort=dateAndTime&_order=desc"));
+
+    if (response.statusCode == 200) {
+      List<dynamic> myRides = json.decode(utf8.decode(response.bodyBytes));
+      List<Ride> rides = myRides.map((e) => Ride.fromJson(e)).toList();
+      setState(() {
+        widget.ridesA.addAll(rides.where((element) => element.state == false && widget.id != element.userId));
+      });
+    }
+  }
+
+  loadFilterFurtherMoto() async {
+    widget.ridesA.clear();
+    final response =
+        await http.get(Uri.parse("http://192.168.1.40:3000/rides?vehicle=Motocicleta&_sort=dateAndTime&_order=desc"));
 
     if (response.statusCode == 200) {
       List<dynamic> myRides = json.decode(utf8.decode(response.bodyBytes));
@@ -248,7 +310,7 @@ class MainPageState extends State<MainPage> {
                   Navigator.of(context).push(
                     PageTransition(
                       child: LoginPage(),
-                      type: PageTransitionType.rightToLeft,
+                      type: PageTransitionType.fade,
                     ),
                   );
                 },
@@ -276,36 +338,98 @@ class MainPageState extends State<MainPage> {
                 return [
                   PopupMenuItem<int>(
                     value: 0,
-                    child: Text("Actualizar")
+                    child: Text("Actualizar", style: TextStyle(fontWeight: FontWeight.bold))
                   ),
                   PopupMenuItem<int>(
                     value: 1,
-                    child: Text("Mostrar por fecha más cercana")
+                    child: Row(
+                      children: [
+                        Text("Fecha más cercana ("),
+                        Icon(Icons.drive_eta, color: Colors.black),
+                        Text(" y "),
+                        Icon(Icons.motorcycle, color: Colors.black),
+                        Text(" )")
+                      ],
+                    )
                   ),
                   PopupMenuItem<int>(
                     value: 2,
-                    child: Text("Mostrar por fecha más lejana")
+                    child: Row(
+                      children: [
+                        Text("  -  "),
+                        Text("Automóvil")
+                      ],
+                    )
+                  ),
+                  PopupMenuItem<int>(
+                    value: 3,
+                    child: Row(
+                      children: [
+                        Text("  -  "),
+                        Text("Motocicleta")
+                      ],
+                    )
+                  ),
+                  PopupMenuItem<int>(
+                    value: 4,
+                    child: Row(
+                      children: [
+                        Text("Fecha más lejana ("),
+                        Icon(Icons.drive_eta, color: Colors.black),
+                        Text(" y "),
+                        Icon(Icons.motorcycle, color: Colors.black),
+                        Text(" )")
+                      ],
+                    )
+                  ),
+                  PopupMenuItem<int>(
+                    value: 5,
+                    child: Row(
+                      children: [
+                        Text("  -  "),
+                        Text("Automóvil")
+                      ],
+                    )
+                  ),
+                  PopupMenuItem<int>(
+                    value: 6,
+                    child: Row(
+                      children: [
+                        Text("  -  "),
+                        Text("Motocicleta")
+                      ],
+                    )
                   ),
                 ];
               },
               onSelected: (value) {
                 if (value == 0) {
                   loadRides();
-                  _controllerSearchStart.clear();
-                  _controllerSearchEnd.clear();
-                  _controllerSearchDate.clear();
+                  clearSearch();
                 }
                 if (value == 1) {
                   loadFilterCloserDate();
-                  _controllerSearchStart.clear();
-                  _controllerSearchEnd.clear();
-                  _controllerSearchDate.clear();
+                  clearSearch();
                 }
                 if (value == 2) {
+                  loadFilterCloserCar();
+                  clearSearch();
+                }
+                if (value == 3) {
+                  loadFilterCloserMoto();
+                  clearSearch();
+                }
+                if (value == 4) {
                   loadFilterFurtherDate();
-                  _controllerSearchStart.clear();
-                  _controllerSearchEnd.clear();
-                  _controllerSearchDate.clear();
+                  clearSearch();
+                }
+                if (value == 5) {
+                  loadFilterFurtherCar();
+                  clearSearch();
+                }
+                if (value == 6) {
+                  loadFilterFurtherMoto();
+                  clearSearch();
                 }
               },
             )
@@ -397,9 +521,7 @@ class MainPageState extends State<MainPage> {
                                   if(widget.ridesA.isEmpty){
                                     showRidesToast();
                                   }
-                                  _controllerSearchStart.clear();
-                                  _controllerSearchEnd.clear();
-                                  _controllerSearchDate.clear();
+                                  clearSearch();
                                 }
                               }
                             )
@@ -439,11 +561,14 @@ class MainPageState extends State<MainPage> {
                                       );
                                     }),
                                     child: SizedBox(
-                                      height: 120,
+                                      height: 140,
                                       child: Column(
                                         crossAxisAlignment: CrossAxisAlignment.end,
                                         children: [
-                                          Icon(widget.ridesA[index].vehicle == 'Motocicleta' ? Icons.motorcycle : Icons.drive_eta, size: 25),
+                                          Icon(
+                                            widget.ridesA[index].vehicle == 'Motocicleta' ? Icons.motorcycle : Icons.drive_eta, 
+                                            size: 25
+                                          ),
                                           Row(
                                             mainAxisAlignment: MainAxisAlignment.start,
                                             children: [
@@ -489,6 +614,10 @@ class MainPageState extends State<MainPage> {
                                               Text(widget.ridesA[index].dateAndTime)
                                             ],
                                           ),
+                                          Icon(
+                                            widget.ridesA[index].stateR == true ? Icons.play_arrow : null,
+                                            size: 20,
+                                          )
                                         ],
                                       ),
                                     ),
